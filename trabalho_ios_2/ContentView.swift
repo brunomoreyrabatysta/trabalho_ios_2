6ad87404b470ee673e1ae72a2ba3573fadda1ee3
Bruno Moreira Batista <39908857+brunomoreyrabatysta@users.noreply.github.com>
2023-11-21 10:19:30 -0300

    Initial Commit

---
+++ trabalho_ios_2/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  trabalho_ios_2
+//
+//  Created by BRUNO MOREIRA BATISTA on 21/11/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ trabalho_ios_2/trabalho_ios_2App.swift
@@ -0,0 +1,17 @@
+//
+//  trabalho_ios_2App.swift
+//  trabalho_ios_2
+//
+//  Created by BRUNO MOREIRA BATISTA on 21/11/23.
+//
+
+import SwiftUI
+
+@main
+struct trabalho_ios_2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
